c2fd714d6a7685f81e6a5da9676ef62b4e284200
Sarang <sarang13579@gmail.com>
2019-10-07 17:17:04 +0530

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -22,7 +22,7 @@ $to = 'sarang@batec-services.com'; // Add your email address inbetween the '' re
 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: info@batec-services.com \n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: mail.batec-services.com  \n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 mail($to2,$email_subject,$email_body,$headers);
